930023b84079bdd548d0d575c27e21b0ed6dc2d7
yash <yashvardhan7906@gmail.com>
2021-12-28 00:08:55 +0530

    4th commit

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 function toggle()
 {
-    let btn=document.getElementById('btn');
     let para=document.getElementById('para');
-    if(para.style.display!='none'){
-        para.style.display!='none';
+    let btn=document.getElementById('btn');
+    if(para.style.display!='none')
+    {
+        para.style.display='none';  
     }
-    else{
-        para.style.display!='block';
+    else
+    {
+        para.style.display='block';
     }
 }
