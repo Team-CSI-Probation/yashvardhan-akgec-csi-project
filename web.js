867042d86c107a96d9de2cdfb617f1a012873d62
yash <yashvardhan7906@gmail.com>
2021-12-27 22:24:20 +0530

    3rd commit

---
+++ web.js
@@ -0,0 +1,11 @@
+function toggle()
+{
+    let btn=document.getElementById('btn');
+    let para=document.getElementById('para');
+    if(para.style.display!='none'){
+        para.style.display!='none';
+    }
+    else{
+        para.style.display!='block';
+    }
+}
